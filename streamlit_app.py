6701ac69fcf5187960e50a4d44b1410972884dec
Dhiraj Singhvi <122069597+dhirajsinghvi@users.noreply.github.com>
2023-01-05 22:21:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('my parents new diner')
-streamlit.header('https://www.emojicopy.com/assets/sprites/joypixels-sprite-40-people.png Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣  Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
