54e9f29ddb276acf85f24cb4d6f6a26d01a714bc
Dhiraj Singhvi <122069597+dhirajsinghvi@users.noreply.github.com>
2023-01-05 22:22:00 -0500

    Update streamlit_app.py
    adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('my parents new diner')
-streamlit.header('🥣  Breakfast Menu')
+streamlit.header('🥣  Breakfast Menu🥣')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
